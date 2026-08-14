d1b31d2880971074b0f4cfbb0e702e2d9b1d06b9
Petteri Castren <petteri.castren@student.oulu.fi>
2026-08-14 22:35:08 +0300

    recipecard to recipe listview

--- lib/screens/recipe_list_screen.dart
+++ lib/screens/recipe_list_screen.dart
@@ -34,13 +34,65 @@ class RecipeListScreen extends StatelessWidget {
             itemCount: recipes.length,
             separatorBuilder: (context, index) => const SizedBox(height: 10),
             // called when new recipe row appears to screen
-            itemBuilder: (context, index){
-              final recipe = recipes[index];
-              return Text(recipe.title);
-            },
+            itemBuilder: (context, index) => _RecipeCard(recipe: recipes[index]),
           );
         },
       ),
     );
   } //build
-} //StatelessWidget
+
+} //RecipeListScreen
+
+/* _ makes this class private */
+class _RecipeCard extends StatelessWidget {
+  final Recipe recipe;
+  const _RecipeCard({required this.recipe});
+
+  @override
+  Widget build(BuildContext context) {
+    // preptime and if no given it is only -. if else
+    final prepTime = recipe.prepTimeMinutes != null
+      ? '${recipe.prepTimeMinutes} min'
+      : '-';
+
+      return Container(
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          border: Border.all(color: Theme.of(context).dividerColor),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Row(
+          children: [
+            Container(
+              width: 56,
+              height: 56,
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.secondaryContainer,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: const Icon(Icons.restaurant),
+            ),
+            const SizedBox(width: 12),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    recipe.title,
+                    style: const TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+                    ),
+                    const SizedBox(height: 2),
+                    Text(
+                      '$prepTime * ${recipe.servings} annosta',
+                      style: TextStyle(fontSize: 12, color: Theme.of(context).hintColor),
+                    ),
+                ],
+              ),
+            ),
+            const Icon(Icons.chevron_right, size: 18),
+          ],
+        ),
+      );
+  } //build
+    
+}//_RecipeCard
